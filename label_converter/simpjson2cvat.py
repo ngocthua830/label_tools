5bf78c11b6b4c37835a09e38c1013d23cfd6aa07
ngocthua830 <ngocthua830@gmail.com>
2023-07-24 12:50:49 +0700

    update simpjson2cvat

--- label_converter/simpjson2cvat.py
+++ label_converter/simpjson2cvat.py
@@ -6,9 +6,9 @@ from pathlib import Path
 def write_class_list(fo, class_list):
     for cls in class_list:
         cls = cls.rstrip()
-        fo.write('\t\t\t<label>\n')
-        fo.write('\t\t\t\t<name>%s</name>\n' %cls)
-        fo.write('\t\t\t</label>\n')
+        fo.write('   <label>\n')
+        fo.write('    <name>%s</name>\n' %cls)
+        fo.write('   </label>\n')
 
 def write_info(fo, class_list):
     now = datetime.now()
@@ -16,16 +16,16 @@ def write_info(fo, class_list):
     
     fo.write('<?xml version="1.0" encoding="utf-8"?>\n')
     fo.write('<annotations>\n')
-    fo.write('\t<version>1.1</version>\n')
-    fo.write('\t<meta>\n')
+    fo.write(' <version>1.1</version>\n')
+    fo.write(' <meta>\n')
     
-    fo.write('\t\t<labels>\n')
+    fo.write('  <labels>\n')
     write_class_list(fo, class_list)
-    fo.write('\t\t</labels>\n')
+    fo.write('  </labels>\n')
     
-    fo.write('\t\t<dumped>%s</dumped>\n' %dt_string)
+    fo.write('  <dumped>%s</dumped>\n' %dt_string)
     
-    fo.write('\t</meta>\n')
+    fo.write(' </meta>\n')
 
 def simpjson2cvat(simpjsonpath, cvatpath):
     fo = open(cvatpath, "w")
@@ -43,7 +43,11 @@ def simpjson2cvat(simpjsonpath, cvatpath):
             jc = json.load(jf)
 #        print(jc)
         
-        fo.write('<image id="%s" name="%s" width="%s" height="%s">\n' %(i, fpath.name, jc["image_width"], jc["image_height"]))
+        fo.write(' <image id="%s" name="%s" width="%s" height="%s">\n' %(i, fpath.name, jc["image_width"], jc["image_height"]))
+        for bbox in jc["bboxes"]:
+            print(bbox)
+            fo.write('  <box label="%s" source="manual" occluded="0" xtl="%s" ytl="%s" xbr="%s" ybr="%s" z_order="0">\n' %(bbox["class_name"], bbox["x1"], bbox["y1"], bbox["x2"], bbox["y2"]))
+        
         for polygon in jc["polygon"]:
             print(polygon)
             points = ""
@@ -51,15 +55,15 @@ def simpjson2cvat(simpjsonpath, cvatpath):
                 x = point["x"]
                 y = point["y"]
                 points += "%s,%s;" %(x, y)
-            fo.write('<polygon label="%s" source="manual" occluded="0" points="%s" z_order="0"></polygon>\n' %(polygon["class_name"], points.rstrip(";")))
-        fo.write('</image>\n')
+            fo.write('  <polygon label="%s" source="manual" occluded="0" points="%s" z_order="0"></polygon>\n' %(polygon["class_name"], points.rstrip(";")))
+        fo.write(' </image>\n')
 
     fo.write('</annotations>')
     fo.close()
 
 if __name__=="__main__":
-    simpjsonpath = Path("page_0_seg_simpjson")
-    cvatpath = Path("page_0_seg_cvat.xml")
+    simpjsonpath = Path("simpjson")
+    cvatpath = Path("cvat.xml")
     
     simpjson2cvat(simpjsonpath, cvatpath)
         
